831caa2535459ac867f18d5a9fcb7a6878f3b2c1
BananaOnTheTree <hoanganhminh2004@gmail.com>
2024-11-12 18:56:00 +0700

    New: Batch Write Thêm batch write vào hệ thống, cải thiện đáng kể thời gian ghi

--- backend/app.go
+++ backend/app.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"math/rand/v2"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/cors"
 	"gorm.io/driver/postgres"
-	_ "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -24,6 +24,8 @@ type URL struct {
 	ID  string `gorm:"primary_key"`
 	URL string `gorm:"not null"`
 }
+
+var urlQueue = make(chan URL, 1000) // Channel hàng đợi với buffer 1000
 
 func main() {
 	RedisClient = redis.NewClient(&redis.Options{
@@ -40,6 +42,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// Khởi tạo worker để xử lý queue
+	go processQueue()
 
 	// Set up the router with CORS
 	router := mux.NewRouter()
@@ -90,33 +95,70 @@ func GetLink(w http.ResponseWriter, r *http.Request) {
 func ShortenURL(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
-	url := r.URL.Query().Get("url")
+	url := r.FormValue("url")
 	if url == "" {
 		w.WriteHeader(400) // Bad Request if no URL is provided
 		return
 	}
 
 	// Generate a new unique ID for the shortened URL
-	newID := makeID(5)
+	newID := makeID(10)
 
-	// Insert the new URL into the database using GORM
+	// Đưa bản ghi vào hàng đợi thay vì ghi trực tiếp vào cơ sở dữ liệu
 	urlRecord := URL{ID: newID, URL: url}
-	if err := DB.Create(&urlRecord).Error; err != nil {
-		w.WriteHeader(500) // Internal Server Error if something goes wrong
-		return
-	}
+	urlQueue <- urlRecord // Đưa vào hàng đợi
 
-	// Return the shortened URL ID
+	// Trả về ID của URL rút gọn ngay lập tức
 	_, _ = w.Write([]byte(newID))
+}
+
+// processQueue xử lý các yêu cầu từ hàng đợi và ghi vào cơ sở dữ liệu
+func processQueue() {
+	batchSize := 500
+	ticker := time.NewTicker(1500 * time.Millisecond)
+	defer ticker.Stop()
+
+	var urls []URL
+
+	for {
+		select {
+		case url := <-urlQueue:
+			urls = append(urls, url)
+
+			// Nếu đạt đến kích thước batch, ghi tất cả vào DB
+			if len(urls) >= batchSize {
+				batchInsert(urls)
+				urls = urls[:0] // Reset lại slice sau khi ghi
+			}
+		case <-ticker.C:
+			// Ghi bất cứ bản ghi nào còn lại vào cuối mỗi giây
+			if len(urls) > 0 {
+				batchInsert(urls)
+				urls = urls[:0]
+			}
+		}
+	}
+}
+
+var totalInserts int
+
+// batchInsert thực hiện batch insert vào cơ sở dữ liệu
+func batchInsert(urls []URL) {
+	if err := DB.Create(&urls).Error; err != nil {
+		// Log lỗi nếu batch insert thất bại
+		println("Batch insert failed:", err.Error())
+	} else {
+		totalInserts += len(urls)
+		println("Total URLs inserted:", totalInserts)
+	}
 }
 
 // makeID generates a random alphanumeric string of the specified length
 func makeID(length int) string {
 	var result string
 	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	const charactersLength = len(characters)
 	for i := 0; i < length; i++ {
-		result += string(characters[rand.IntN(charactersLength)])
+		result += string(characters[rand.Intn(len(characters))])
 	}
 	return result
 }
